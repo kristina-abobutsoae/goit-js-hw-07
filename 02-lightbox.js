df27e0e68d1573f141366946f5e737425b7f3ed0
kristina-abobutsoae <abobutsoaekristina@gmail.com>
2023-07-25 22:31:48 +0200

    commit

--- 02-lightbox.js
+++ 02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+setGalleryHtml("beforeend", galleryElCreateMarkup(galleryItems));
+
+function galleryElCreateMarkup(galleryItems){
+    return galleryItems
+    .map(({ preview,original,description}) => {
+       return `<li class = "gallery__item">
+              <a class ="gallery__link" href="${original}">
+              <img
+              class ="gallery__image"
+              src="${preview}"
+              alt="${description}"
+              />
+              </a>
+       </li>`;
+    })
+    .join("");
+}
+
+function setGalleryHtml(position, gallery) {
+  galleryList.insertAdjacentHTML(position, gallery);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: "bottom",
+  download: "Download",
+
+});
+
+console.log(galleryItems);
